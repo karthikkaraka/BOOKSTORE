615ff885f2aeee485d7e0202009af7079ec6450b
karthikkaraka <karthikkaraka444@gmail.com>
2025-08-24 10:36:08 +0530

    added if no user found case

--- src/main/java/com/karthik/BOOKSTORE/Controller/UserController.java
+++ src/main/java/com/karthik/BOOKSTORE/Controller/UserController.java
@@ -37,7 +37,7 @@ public class UserController {
         {
             return jwtservice.generatejwttoken(user);
         }
-        else return null;
+        else return "no user found";
     }
 
 }
